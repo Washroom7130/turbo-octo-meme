337c82d1dc709f0c0cad1a433808cdeb0b9fab35
bruh <bruh@dumb.myguest.virtualbox.org>
2025-07-01 11:22:15 +0700

    Update SuKien routes

--- src/main/java/com/api/Event_Management_API/controller/sukien/SuKienController.java
+++ src/main/java/com/api/Event_Management_API/controller/sukien/SuKienController.java
@@ -1,5 +1,37 @@
 package com.api.Event_Management_API.controller.sukien;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.Event_Management_API.dto.SuKien.CUSuKienRequest;
+import com.api.Event_Management_API.service.SuKienService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/sukien")
 public class SuKienController {
     
+    @Autowired
+    private SuKienService suKienService;
+
+    @PreAuthorize("hasAnyAuthority('NhanVien', 'QuanLy')")
+    @PostMapping("/add")
+    public ResponseEntity<?> addSuKien(@Valid @ModelAttribute CUSuKienRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            String errorMessage = result.getFieldErrors().get(0).getDefaultMessage();
+            return ResponseEntity.badRequest().body(Map.of("error", errorMessage));
+        }
+
+        return suKienService.addSuKien(request);
+    }
 }
--- src/main/java/com/api/Event_Management_API/service/SuKienService.java
+++ src/main/java/com/api/Event_Management_API/service/SuKienService.java
@@ -1,5 +1,25 @@
 package com.api.Event_Management_API.service;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.api.Event_Management_API.dto.SuKien.CUSuKienRequest;
+import com.api.Event_Management_API.util.FileUploadUtil;
+
+@Service
 public class SuKienService {
-    
+    public ResponseEntity<?> addSuKien(CUSuKienRequest request) {
+        try {
+            String imagePath = FileUploadUtil.saveImageFile(request.getAnhSuKien());
+
+            return ResponseEntity.ok(Map.of("imagePath", imagePath));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body(Map.of("error", "Can't save file"));
+        }
+    }
 }
