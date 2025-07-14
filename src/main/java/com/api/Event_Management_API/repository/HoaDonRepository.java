6bdb3e75eba8fbce7e3a7a958e034437c2432db3
bruh <bruh@dumb.myguest.virtualbox.org>
2025-07-14 11:49:26 +0700

    Add HoaDon scheduler

--- src/main/java/com/api/Event_Management_API/repository/HoaDonRepository.java
+++ src/main/java/com/api/Event_Management_API/repository/HoaDonRepository.java
@@ -1,5 +1,8 @@
 package com.api.Event_Management_API.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +11,5 @@ import com.api.Event_Management_API.model.HoaDon;
 
 public interface HoaDonRepository extends JpaRepository<HoaDon, String> {
     Page<HoaDon> findAllByMaKhachHang(Integer maKhachHang, Pageable pageable);
+    List<HoaDon> findByTrangThaiHoaDonAndThoiGianHieuLucBefore(String status, LocalDateTime now);
 }
